a58774c8ddc840b80ce0f0582f9e5eb4256b5af0
Wallace Vieira <wallaceofc@hotmail.com>
2022-11-05 15:45:09 -0300

    Refatorando, adicionando animação, adicionando variações combinações de cor

--- index.js
+++ index.js
@@ -20,9 +20,6 @@ function createCard(date, day, games) {
 }
 
 document.querySelector("#app").innerHTML = `
-<header>
-            <img src="./assets/logo.svg" alt="Logo da NLW">
-        </header>
         <main id="cards">
           ${createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia") + createGame("brazil", "16:00", "serbia") + createGame("brazil", "16:00", "serbia"))}
           ${createCard("28/11", "segunda", createGame("brazil", "13:00", "switzerland") + createGame("brazil", "16:00", "serbia") + createGame("brazil", "16:00", "serbia"))}
